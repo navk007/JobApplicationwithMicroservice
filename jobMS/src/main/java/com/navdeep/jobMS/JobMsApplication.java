e3c355327f27f142ae563651382c130f63a0c7be
navk007 <534navdeep@gmail.com>
2025-12-23 01:41:35 +0530

    Added OpenFeignClient to remove the extra boilerplate code for restTemplate (calls to other microservice for data).

--- jobMS/src/main/java/com/navdeep/jobMS/JobMsApplication.java
+++ jobMS/src/main/java/com/navdeep/jobMS/JobMsApplication.java
@@ -2,12 +2,12 @@ package com.navdeep.jobMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class JobMsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JobMsApplication.class, args);
 	}
-
 }
---
+++ jobMS/src/main/java/com/navdeep/jobMS/job/clients/ReviewClient.java
@@ -0,0 +1,16 @@
+package com.navdeep.jobMS.job.clients;
+
+import com.navdeep.jobMS.job.external.Review;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient("REVIEWMS")
+public interface ReviewClient {
+
+    @GetMapping("/reviews")
+    List<Review> getReviews(@RequestParam("companyId") Long companyId);
+}
