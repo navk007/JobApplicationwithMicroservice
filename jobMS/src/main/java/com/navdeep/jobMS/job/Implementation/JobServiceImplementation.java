5a3eefa59fdf91153ca5e71ddca401d463742f67
navk007 <534navdeep@gmail.com>
2025-12-23 01:41:35 +0530

    Added OpenFeignCleint to remove the extra boilerplate code for restTemplate (calls to other microservice for data).

--- jobMS/src/main/java/com/navdeep/jobMS/job/Implementation/JobServiceImplementation.java
+++ jobMS/src/main/java/com/navdeep/jobMS/job/Implementation/JobServiceImplementation.java
@@ -3,6 +3,8 @@ package com.navdeep.jobMS.job.Implementation;
 import com.navdeep.jobMS.job.Job;
 import com.navdeep.jobMS.job.JobRepository;
 import com.navdeep.jobMS.job.JobService;
+import com.navdeep.jobMS.job.clients.CompanyClient;
+import com.navdeep.jobMS.job.clients.ReviewClient;
 import com.navdeep.jobMS.job.dto.JobDTO;
 import com.navdeep.jobMS.job.external.Company;
 import com.navdeep.jobMS.job.external.Review;
@@ -22,23 +24,32 @@ import java.util.Optional;
 public class JobServiceImplementation implements JobService {
     JobRepository jobRepository;
 
+    private CompanyClient companyClient;
+    private ReviewClient reviewClient;
+
     @Autowired
     RestTemplate restTemplate;
 
-    public JobServiceImplementation(JobRepository jobRepository) {
+    public JobServiceImplementation(JobRepository jobRepository, CompanyClient companyClient, ReviewClient reviewClient) {
         this.jobRepository = jobRepository;
+        this.companyClient = companyClient;
+        this.reviewClient = reviewClient;
     }
 
     private JobDTO convertToDto(Job job){
+//        Before OpenFeignCleint Integration
+//        Company company = restTemplate.getForObject("http://COMPANYMS:8081/companies/" + job.getCompanyId(), Company.class);
+//        ResponseEntity<List<Review>> reviewResponse=restTemplate.exchange(
+//                "http://REVIEWMS:8083/reviews?companyId=" + job.getCompanyId(),
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<List<Review>>(){
+//                });
+//        List<Review> reviews=reviewResponse.getBody();
 
-        Company company = restTemplate.getForObject("http://COMPANYMS:8081/companies/" + job.getCompanyId(), Company.class);
-        ResponseEntity<List<Review>> reviewResponse=restTemplate.exchange(
-                "http://REVIEWMS:8083/reviews?companyId=" + job.getCompanyId(),
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<Review>>(){
-                });
-        List<Review> reviews=reviewResponse.getBody();
+//        After OpenFeignCleint Integration
+        Company company = companyClient.getCompany(job.getCompanyId());
+        List<Review> reviews=reviewClient.getReviews(job.getCompanyId());
 
         JobDTO jobDTO =JobMapper.mapToJobWithCompanyDto(job, company, reviews);
 
